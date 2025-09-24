1649d5f6058f0b2fafe3930963b42f69e0379083
Silas Álefe <116173586+SilasAlefe@users.noreply.github.com>
2025-09-24 16:48:02 -0300

    24/09/2025-16:47

---
+++ Machine-Learning-e-Data-Science/Expressões-regulares.py
@@ -0,0 +1,11 @@
+import re
+
+#frase = 'Olá, meu número de telefone é (42)00010-0000'
+#print(re.search(r'\(\d{2}\)\d{4,5}-\d{4}', frase))
+
+#frase = 'A placa de carro que eu anotei durante o acidente foi FRT-1998'
+#print(re.search(r'\w{3}-\d{4}', frase)) #ou r'[A-Za-z]{3}-\d{4}'
+
+#email = 'Entre em contato, meu email é teste@teste.com'
+#print(re.search(r'\w+@\w+\.\w{3}', email))
+
